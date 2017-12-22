9b9800890b2400f8abedcbb29d7c4d9a8a489ed2
Eli Nunez <enunez025@gmail.com>
2017-12-22 16:03:55 +0000

    navbar edit

--- src/components/navbar/main.js
+++ src/components/navbar/main.js
@@ -2,14 +2,14 @@ var nav = document.querySelector('#navbar');
 
 nav.innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">ENUN03</a>
+      <a class="navbar-brand" href="index.html">ENUN03</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="projects.html">Projects</a>
