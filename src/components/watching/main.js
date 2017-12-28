573bd165e03572bfaa60ad467ae529d2d537bfb8
enun03 <enunez025@gmail.com>
2017-12-28 19:00:05 -0400

    watch component double column layout

--- src/components/watching/main.js
+++ src/components/watching/main.js
@@ -18,20 +18,41 @@ var Watching = {
     xhr.send();
   },
   render : () => {
+    var output = '';
+
+    Watching.resData.reverse();
     console.log('Enun03 Watching Data');
     console.log(Watching.resData);
-    this.watchingDOM.innerHTML = `<h3>Recent Activity:</h3>`;
-    Watching.resData.reverse();
-    Watching.resData.forEach( w => {
-      this.watchingDOM.innerHTML += `
-        <div class="card" style="margin: 0.3em;">
-          <div class="card-body">
-            <a href="${w.html_url}"><p>${w.name}</p></a>
+
+    this.watchingDOM.innerHTML = `<h3 style="margin-top:0.35em;">Recent Activity:</h3>`;
+    
+    Watching.resData.forEach( (w, i) => {
+      if(isEven(i) === true) output += `<div class="row">`;
+      output += `
+        <div class="col-md-6">
+          <div class="card" style="margin: 0.2em;">
+            <div class="card-body">
+              <a href="${w.html_url}"><p>${w.name}</p></a>
+              <p style="font-size: 12px;">last update: ${new Date(w.updated_at).toDateString()}</p>
+            </div>
           </div>
         </div>
       `;
+      debugger;
+      if(i !== 0 && isOdd(i) === true) output += `</div>`;
     });
+    output += `</div>`;
+    console.log(output);
+    this.watchingDOM.innerHTML += output;
   }
+}
+
+function isEven(n) {
+  return n % 2 == 0;
+}
+
+function isOdd(n) {
+  return Math.abs(n % 2) == 1;
 }
 
 Watching.data();
