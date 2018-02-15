abbbc96295f6945f187faaf0d7b575350ed5614b
enun03 <enunez025@gmail.com>
2018-02-15 02:22:39 -0400

    demo link added

--- src/components/navbar/main.js
+++ src/components/navbar/main.js
@@ -2,7 +2,7 @@ var navOutput = document.querySelector('#navbar');
 
 navOutput.innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="index.html"><i class="fas fa-code" style="color: #555;"></i> Dev Portfolio</a>
+      <a class="navbar-brand" href="/"><i class="fas fa-code" style="color: #555;"></i> Dev Portfolio</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
